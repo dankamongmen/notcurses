32394dd892f4e1329704842fead713a9920f3f76
nick black <dankamongmen@gmail.com>
2019-11-24 15:26:01 -0500

    egcpool: preserve incoming EGC on realloc

--- src/lib/egcpool.c
+++ src/lib/egcpool.c
@@ -3,15 +3,7 @@
 
 #define POOL_MINIMUM_ALLOC BUFSIZ
 
-int egcpool_grow(egcpool* pool, size_t len, bool force){
-  const size_t poolfree = pool->poolsize - pool->poolused;
-  // proactively get more space if we have less than 10% free. this doesn't
-  // guarantee that we'll have enough space to insert the string -- we could
-  // theoretically have every 10th byte free, and be unable to write even a
-  // two-byte egc -- so we might have to allocate after an expensive search :/.
-  if(poolfree >= len && poolfree * 10 > pool->poolsize && !force){
-    return 0;
-  }
+int egcpool_grow(egcpool* pool, size_t len){
   size_t newsize = pool->poolsize * 2;
   if(newsize < POOL_MINIMUM_ALLOC){
     newsize = POOL_MINIMUM_ALLOC;
