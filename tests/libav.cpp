037adc957139d5386ecbcee8dc82c9d3590a4f73
nick black <dankamongmen@gmail.com>
2019-11-24 22:22:18 -0500

    add notcurses_image_open() #23

---
+++ tests/libav.cpp
@@ -0,0 +1,28 @@
+#include <notcurses.h>
+#include "main.h"
+
+class LibavTest : public :: testing::Test {
+ protected:
+  void SetUp() override {
+    notcurses_options nopts{};
+    nopts.outfd = STDIN_FILENO;
+    nc_ = notcurses_init(&nopts);
+    ASSERT_NE(nullptr, nc_);
+    n_ = notcurses_stdplane(nc_);
+    ASSERT_NE(nullptr, n_);
+  }
+
+  void TearDown() override {
+    if(nc_){
+      EXPECT_EQ(0, notcurses_stop(nc_));
+    }
+  }
+
+  struct notcurses* nc_{};
+  struct ncplane* n_{};
+};
+
+TEST_F(LibavTest, LoadImage) {
+  int ret = notcurses_image_open(nc_, "../tools/dsscaw-purp.png");
+  ASSERT_EQ(0, ret);
+}
