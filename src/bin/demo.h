496efa7712a5259f11a4227b1f98dccf41a0d2e3
nick black <dankamongmen@gmail.com>
2019-11-25 18:10:07 -0500

    run maxcolor demo

--- src/bin/demo.h
+++ src/bin/demo.h
@@ -12,6 +12,7 @@ extern "C" {
 int unicodeblocks_demo(struct notcurses* nc);
 int widecolor_demo(struct notcurses* nc);
 int box_demo(struct notcurses* nc);
+int maxcolor_demo(struct notcurses* nc);
 int grid_demo(struct notcurses* nc);
 
 #ifdef __cplusplus
