dce27ecb0a0122ec46f9b1a2338adf2865636db4
nick black <dankamongmen@gmail.com>
2019-11-25 09:46:49 -0500

    render stats #12

--- src/lib/timespec.h
+++ src/lib/timespec.h
@@ -1,14 +1,22 @@
-#ifndef NOTCURSES_EGCPOOL
-#define NOTCURSES_EGCPOOL
+#ifndef NOTCURSES_TIMESPEC
+#define NOTCURSES_TIMESPEC
 
 #include <time.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int timespec_subtract(struct timespec *result, const struct timespec *time0,
-                      struct timespec *time1);
+int timespec_subtract(struct timespec *result, const struct timespec *time1,
+                      struct timespec *time0);
+
+static inline int64_t
+timespec_subtract_ns(const struct timespec* time1, const struct timespec* time0){
+  int64_t ns = time1->tv_sec * 1000000000 + time1->tv_nsec;
+  ns -= time0->tv_sec * 1000000000 + time0->tv_nsec;
+  return ns;
+}
 
 #ifdef __cplusplus
 }
