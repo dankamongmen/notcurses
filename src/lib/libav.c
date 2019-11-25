66432c37e100dd85922b9ece632f083aec0e3dad
nick black <dankamongmen@gmail.com>
2019-11-24 23:56:14 -0500

    libav: extract frame from AVFormatContext

--- src/lib/libav.c
+++ src/lib/libav.c
@@ -9,6 +9,23 @@ int notcurses_image_open(struct notcurses* nc, const char* filename){
     fprintf(stderr, "Couldn't open %s (%s)\n", filename, av_err2str(ret));
     return ret;
   }
+  if((ret = avformat_find_stream_info(ps, NULL)) < 0){
+    fprintf(stderr, "Error extracting stream info from %s (%s)\n", filename,
+            av_err2str(ret));
+    avformat_free_context(ps);
+    return ret;
+  }
+  // av_dump_format(ps, 0, filename, false);
+  AVPacket* packet = av_packet_alloc();
+  if((ret = av_read_frame(ps, packet)) < 0){
+    fprintf(stderr, "Error reading frame info from %s (%s)\n", filename,
+            av_err2str(ret));
+    av_packet_free(&packet);
+    avformat_free_context(ps);
+    return -1;
+  }
+  // FIXME
+  av_packet_free(&packet);
   avformat_free_context(ps);
   return 0;
 }
