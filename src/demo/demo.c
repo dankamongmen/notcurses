c5ecc28ca10028f5c23e6f88e3a733390cd50d0e
nick black <dankamongmen@gmail.com>
2019-12-03 23:40:18 -0500

    remove resize check from demo core #105

--- src/demo/demo.c
+++ src/demo/demo.c
@@ -189,7 +189,6 @@ intro(struct notcurses* nc){
 
 static int
 ext_demos(struct notcurses* nc, const char* demos){
-  int dimy, dimx;
   while(*demos){
     int ret = 0;
     switch(*demos){
@@ -209,7 +208,6 @@ ext_demos(struct notcurses* nc, const char* demos){
       return ret;
     }
     ++demos;
-    notcurses_resize(nc, &dimy, &dimx);
   }
   return 0;
 }
