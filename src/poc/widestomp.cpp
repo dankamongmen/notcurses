2378b8da29881ffa01ffc30b64d06ed361cebc4a
nick black <dankamongmen@gmail.com>
2019-12-31 22:04:03 -0500

    develop out widestomp PoC

--- src/poc/widestomp.cpp
+++ src/poc/widestomp.cpp
@@ -11,8 +11,16 @@ int main(void){
   struct notcurses* nc = notcurses_init(&opts, stdout);
   struct ncplane* n = notcurses_stdplane(nc);
 
+  ncplane_set_fg_rgb(n, 0x80, 0xc0, 0x80);
+  ncplane_set_bg_rgb(n, 0x00, 0x40, 0x00);
   ncplane_putstr(n, "\u5168");
   ncplane_putstr(n, "\u5f62");
+  notcurses_render(nc);
+  sleep(1);
+  struct ncplane* nn = ncplane_new(nc, 1, 2, 0, 1, nullptr);
+  ncplane_set_fg_rgb(nn, 0xc0, 0x80, 0xc0);
+  ncplane_set_bg_rgb(nn, 0x20, 0x00, 0x20);
+  ncplane_putstr(nn, "AB");
   notcurses_render(nc);
   sleep(1);
   cell c = CELL_TRIVIAL_INITIALIZER;
