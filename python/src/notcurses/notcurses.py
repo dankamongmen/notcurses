9f103d36e270e06dc426c4a9bf07c9a77eab214b
nick black <dankamongmen@gmail.com>
2020-01-07 14:21:54 -0500

    python: get init/stop working

--- python/src/notcurses/notcurses.py
+++ python/src/notcurses/notcurses.py
@@ -1,17 +1,20 @@
+import sys
+import locale
 from _notcurses import lib, ffi
 
 class Notcurses:
     def __init__(self):
-        self.nc = lib.notcurses_init()
+        opts = ffi.new("notcurses_options *")
+        self.nc = lib.notcurses_init(opts, sys.stdout)
 
     def __del__(self):
         lib.notcurses_stop(self.nc)
 
-    def render():
+    def render(self):
         return lib.notcurses_render(self.nc)
 
-def main():
-    print('ohhhh yeah')
+if __name__ == '__main__':
+    locale.setlocale(locale.LC_ALL, "")
     nc = Notcurses()
     nc.render()
     print('ohhhh yeah')
