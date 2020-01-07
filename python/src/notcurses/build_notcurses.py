5abbce04d478651d2aa0a1b248092618b522108b
nick black <dankamongmen@gmail.com>
2020-01-07 14:21:54 -0500

    python: get init/stop working

--- python/src/notcurses/build_notcurses.py
+++ python/src/notcurses/build_notcurses.py
@@ -10,8 +10,51 @@ ffibuild.set_source(
 )
 
 ffibuild.cdef("""
-struct notcurses_options;
-struct notcurses* notcurses_init(const struct notcurses_options*, FILE*);
+typedef enum {
+  NCLOGLEVEL_SILENT,  // default. print nothing once fullscreen service begins
+  NCLOGLEVEL_PANIC,   // print diagnostics immediately related to crashing
+  NCLOGLEVEL_FATAL,   // we're hanging around, but we've had a horrible fault
+  NCLOGLEVEL_ERROR,   // we can't keep doin' this, but we can do other things
+  NCLOGLEVEL_WARNING, // you probably don't want what's happening to happen
+  NCLOGLEVEL_INFO,    // "standard information"
+  NCLOGLEVEL_VERBOSE, // "detailed information"
+  NCLOGLEVEL_DEBUG,   // this is honestly a bit much
+  NCLOGLEVEL_TRACE,   // there's probably a better way to do what you want
+} ncloglevel_e;
+typedef struct notcurses_options {
+  // The name of the terminfo database entry describing this terminal. If NULL,
+  // the environment variable TERM is used. Failure to open the terminal
+  // definition will result in failure to initialize notcurses.
+  const char* termtype;
+  // If smcup/rmcup capabilities are indicated, notcurses defaults to making
+  // use of the "alternate screen". This flag inhibits use of smcup/rmcup.
+  bool inhibit_alternate_screen;
+  // By default, we hide the cursor if possible. This flag inhibits use of
+  // the civis capability, retaining the cursor.
+  bool retain_cursor;
+  // Notcurses does not clear the screen on startup unless thus requested to.
+  bool clear_screen_start;
+  // Notcurses typically prints version info in notcurses_init() and performance
+  // info in notcurses_stop(). This inhibits that output.
+  bool suppress_banner;
+  // We typically install a signal handler for SIG{INT, SEGV, ABRT, QUIT} that
+  // restores the screen, and then calls the old signal handler. Set to inhibit
+  // registration of these signal handlers.
+  bool no_quit_sighandlers;
+  // We typically install a signal handler for SIGWINCH that generates a resize
+  // event in the notcurses_getc() queue. Set to inhibit this handler.
+  bool no_winch_sighandler;
+  // If non-NULL, notcurses_render() will write each rendered frame to this
+  // FILE* in addition to outfp. This is used primarily for debugging.
+  FILE* renderfp;
+  // Progressively higher log levels result in more logging to stderr. By
+  // default, nothing is printed to stderr once fullscreen service begins.
+  ncloglevel_e loglevel;
+} notcurses_options;
+struct notcurses* notcurses_init(const notcurses_options*, FILE*);
 int notcurses_stop(struct notcurses*);
 int notcurses_render(struct notcurses*);
 """)
+
+if __name__ == "__main__":
+    ffibuild.compile(verbose=True)
