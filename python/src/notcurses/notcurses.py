8f6afb8f9ad551b49246de069bac96da88d15b3a
nick black <dankamongmen@gmail.com>
2020-01-09 01:14:48 -0500

    python: notcurses_stdplane()

--- python/src/notcurses/notcurses.py
+++ python/src/notcurses/notcurses.py
@@ -2,10 +2,16 @@ import sys
 import locale
 from _notcurses import lib, ffi
 
+class Ncplane:
+    def __init__(self, plane):
+        self.n = plane
+
 class Notcurses:
     def __init__(self):
         opts = ffi.new("notcurses_options *")
+        #opts.inhibit_alternate_screen = True
         self.nc = lib.notcurses_init(opts, sys.stdout)
+        self.stdplane = lib.notcurses_stdplane(self.nc)
 
     def __del__(self):
         lib.notcurses_stop(self.nc)
@@ -13,8 +19,10 @@ class Notcurses:
     def render(self):
         return lib.notcurses_render(self.nc)
 
+    def stdplane(self):
+        return self.stdplane
+
 if __name__ == '__main__':
     locale.setlocale(locale.LC_ALL, "")
     nc = Notcurses()
     nc.render()
-    print('ohhhh yeah')
