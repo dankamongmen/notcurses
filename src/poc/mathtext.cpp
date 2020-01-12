b8e11e5ef46167cf6eb885fcf663b0bfd62552bb
nick black <dankamongmen@gmail.com>
2020-01-12 01:37:28 -0500

    poc mathtext works now #260

--- src/poc/mathtext.cpp
+++ src/poc/mathtext.cpp
@@ -11,6 +11,8 @@ int mathtext(struct notcurses* nc){
   struct ncplane* n = ncplane_new(nc, HEIGHT, WIDTH, dimy - HEIGHT - 1, dimx - WIDTH - 1, NULL);
   if(n){
     struct ncplane* stdn = notcurses_stdplane(nc);
+    ncplane_set_fg(n, 0xffffff);
+    ncplane_set_bg(n, 0x008000);
     ncplane_printf_aligned(n, 0, NCALIGN_RIGHT, "∮E⋅da=Q,n→∞,∑f(i)=∏g(i)⎧⎡⎛┌─────┐⎞⎤⎫");
     ncplane_printf_aligned(n, 1, NCALIGN_RIGHT, "⎪⎢⎜│a²+b³ ⎟⎥⎪");
     ncplane_printf_aligned(n, 2, NCALIGN_RIGHT, "∀x∈ℝ:⌈x⌉=−⌊−x⌋,α∧¬β=¬(¬α∨β)⎪⎢⎜│───── ⎟⎥⎪");
@@ -33,6 +35,39 @@ int main(void){
   struct notcurses* nc = notcurses_init(&opts, stdout);
   if(nc == nullptr){
     return EXIT_FAILURE;
+  }
+  const char c[] =
+"Jegkanspiseglassutenåskademeg"
+"Egkannetaglasskaðaleysur"
+"Éggetetiðgleránþessaðmeiðamig"
+"𝐸=𝑚𝑐²"
+"Jagkanätaglasutanattskadamig"
+"Jegkanspiseglasdetgørikkeondtpåmig"
+"㎚㎛㎜㎝㎞㎟㎠㎡㎢㎣㎤㎥㎦㎕㎖㎗㎘㏄㎰㎱㎲㎳㎍㎎㎏㎅㎆㏔㎇㎐㎑㎒㎓㎔㎮㎯"
+"Ækaæeglassuhenatdetgomænaue"
+"က္ယ္ဝန္တော၊က္ယ္ဝန္မမ္ယက္စားနုိင္သည္။၎က္ရောင္ထိခုိက္မ္ဟုမရ္ဟိပာ။"
+"ကျွန်တောကျွန်မမှန်စားနိုင်တယ်။၎င်းကြေင့်ထိခိုက်မှုမရှိပါ။"
+"Tôicóthểănthủytinhmàkhônghạigì"
+"些𣎏世咹水晶𦓡空𣎏害"
+"ខ្ញុំអាចញុំកញ្ចក់បានដោយគ្មានបញ្ហា"
+"ຂອ້ຍກິນແກ້ວໄດ້ໂດຍທີ່ມັນບໍ່ໄດ້ເຮັດໃຫ້ຂອ້ຍເຈັບ"
+"ฉันกินกระจกได้แต่มันไม่ทำให้ฉันเจ็"
+"Бишилидэйчаднанададхортойби"
+"ᠪᠢᠰᠢᠯᠢᠢᠳᠡᠶᠦᠴᠢᠳᠠᠨᠠ᠂ᠨᠠᠳᠤᠷᠬᠣᠤᠷᠠᠳᠠᠢᠪᠢᠰ"
+"मकाँचखानसक्छूरमलाईकेहिनीहुन्न्"
+"ཤེལ་སྒོ་ཟ་ནས་ང་ན་གི་མ་རེད"
+"我能吞下玻璃而不伤身体"
+"我能吞下玻璃而不傷身體"
+"Góaē-tàngchia̍hpo-lêmābētio̍h-siong"
+"私はガラスを食べられますそれは私を傷つけません"
+"나는유리를먹을수있어요.그래도아프지않아"
+"Misavekakaeglasheminosavekatemmi"
+"Hikiiaʻukeʻaiikeaniani;ʻaʻolenōlāaueʻeha"
+"Ekoʻanaekaiitekarahimeaʻāʻaʻehauhau"
+"ᐊᓕᒍᖅᓂᕆᔭᕌᖓᒃᑯᓱᕋᙱᑦᑐᓐᓇᖅᑐ";
+  struct ncplane* n = notcurses_stdplane(nc);
+  for(int i = 0 ; i < 25 ; ++i){
+    ncplane_putstr(n, c);
   }
   if(mathtext(nc)){
     notcurses_stop(nc);
