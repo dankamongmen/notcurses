1b4ebd3fb849c3b39a18dc6f7ee47a09d04c5d25
nick black <dankamongmen@gmail.com>
2020-03-15 02:12:27 -0400

    more tetris

--- src/tetris/main.cpp
+++ src/tetris/main.cpp
@@ -1,13 +1,19 @@
+#include <iostream>
+#include <mutex>
 #include <thread>
+#include <chrono>
 #include <cstdlib>
 #include <clocale>
 #include <ncpp/NotCurses.hh>
+
+using namespace std::chrono_literals;
 
 class Tetris {
 public:
   Tetris(ncpp::NotCurses& nc) :
     nc_(nc),
-    score_(0)
+    score_(0),
+    msdelay_(10ms)
   {
     // FIXME draw board
   }
@@ -18,16 +24,26 @@ public:
 
   // FIXME ideally this would be called from constructor :/
   void Ticker(){
-    // FIXME check whether we ought die
-    while(true){
-      // FIXME get value from level
-      std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
+    std::chrono::milliseconds ms;
+    do{
+      mtx_.lock();
+      ms = msdelay_;
+      mtx_.unlock();
+      std::this_thread::sleep_for(ms);
+    }while(ms != std::chrono::milliseconds::zero());
+  }
+
+  void Stop(){
+    mtx_.lock();
+    msdelay_ = std::chrono::milliseconds::zero(); // FIXME wake it up?
+    mtx_.unlock();
   }
 
 private:
   ncpp::NotCurses& nc_;
   uint64_t score_;
+  std::mutex mtx_;
+  std::chrono::milliseconds msdelay_;
 
 };
 
@@ -38,7 +54,23 @@ int main(void){
   notcurses_options ncopts{};
   ncpp::NotCurses nc(ncopts);
   Tetris t{nc};
-  std::thread tid(&Tetris::Ticker, t);
-  while(true); // FIXME spin on keyboard input, play game
+  std::thread tid(&Tetris::Ticker, &t);
+  char32_t input;
+  ncinput ni;
+  while((input = nc.getc(true, &ni)) != (char32_t)-1){
+    if(input == 'q'){
+      break;
+    }
+    switch(input){
+      case NCKEY_LEFT: break;
+      case NCKEY_RIGHT: break;
+    }
+  }
+  if(input == 'q'){
+    t.Stop();
+    tid.join();
+  }else{
+    return EXIT_FAILURE;
+  }
   return nc.stop() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
