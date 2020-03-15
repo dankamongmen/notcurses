fdf03ed97b402a006a310d6501e82233cbe1c6fd
nick black <dankamongmen@gmail.com>
2020-03-15 01:29:21 -0400

    tetris basic skeleton

--- src/tetris/main.cpp
+++ src/tetris/main.cpp
@@ -1,6 +1,33 @@
 #include <cstdlib>
-#include <notcurses/notcurses.h>
+#include <clocale>
+#include <ncpp/NotCurses.hh>
+
+class Tetris {
+public:
+  Tetris(ncpp::NotCurses& nc) :
+    nc_(nc),
+    score_(0)
+  {
+    // FIXME draw board
+  }
+
+  // 0.5 cell aspect: One board height == one row. One board width == two columns.
+  static constexpr auto BOARD_WIDTH = 10;
+  static constexpr auto BOARD_HEIGHT = 20;
+
+private:
+  ncpp::NotCurses& nc_;
+  uint64_t score_;
+
+};
 
 int main(void){
-  return EXIT_SUCCESS;
+  if(setlocale(LC_ALL, "") == nullptr){
+    return EXIT_FAILURE;
+  }
+  notcurses_options ncopts{};
+  ncpp::NotCurses nc(ncopts);
+  Tetris t{nc};
+  // FIXME play game
+  return nc.stop() ? EXIT_FAILURE : EXIT_SUCCESS;
 }
