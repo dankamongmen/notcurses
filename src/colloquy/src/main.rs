ddf206462b7e6b089d66e595d49e14745bb84177
nick black <dankamongmen@gmail.com>
2020-03-27 05:51:52 -0400

    colloquy: branch on widget type

--- src/colloquy/src/main.rs
+++ src/colloquy/src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     use clap::{load_yaml, App};
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+    if matches.is_present("msgbox") {
+        // do a messagebox
+    }else{
+        eprintln!("Needed a widget type");
+        std::process::exit(1);
+    }
 }
