6c81cb3b454c3e5a023fd2cc7bf629278d37bac5
nick black <dankamongmen@gmail.com>
2020-03-27 06:27:26 -0400

    rust: use libnotcurses-sys, add test

--- rust/notcurses/src/lib.rs
+++ rust/notcurses/src/lib.rs
@@ -1,7 +1,33 @@
+extern crate libnotcurses_sys as ffi;
+
+extern {
+    fn libc_stdout() -> *mut ffi::_IO_FILE;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn create_context() {
+        unsafe {
+            let _ = libc::setlocale(libc::LC_ALL, std::ffi::CString::new("").unwrap().as_ptr());
+            let opts: ffi::notcurses_options = ffi::notcurses_options {
+                inhibit_alternate_screen: false,
+                loglevel: 0,
+                termtype: std::ptr::null(),
+                retain_cursor: false,
+                suppress_banner: false,
+                no_winch_sighandler: false,
+                no_quit_sighandlers: false,
+                renderfp: std::ptr::null_mut(),
+                margin_t: 0,
+                margin_r: 0,
+                margin_b: 0,
+                margin_l: 0,
+            };
+            let nc = ffi::notcurses_init(&opts, libc_stdout());
+            ffi::notcurses_stop(nc);
+        }
     }
 }
--- src/colloquy/src/main.rs
+++ src/colloquy/src/main.rs
@@ -8,4 +8,22 @@ fn main() {
         eprintln!("Needed a widget type");
         std::process::exit(1);
     }
+
+    let _ = libc::setlocale(libc::LC_ALL, std::ffi::CString::new("").unwrap().as_ptr());
+    let opts: notcurses_options = libnotcurses-sys::notcurses_options {
+        inhibit_alternate_screen: true,
+        loglevel: 0,
+        termtype: std::ptr::null(),
+        retain_cursor: false,
+        suppress_banner: false,
+        no_winch_sighandler: false,
+        no_quit_sighandlers: false,
+        renderfp: std::ptr::null_mut(),
+        margin_t: 0,
+        margin_r: 0,
+        margin_b: 0,
+        margin_l: 0,
+    };
+    let nc = notcurses_init(&opts, libc_stdout());
+    notcurses_stop(nc);
 }
