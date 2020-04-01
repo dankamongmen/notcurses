5795fe02399c1f92fd952e656ec1fdb9a04bd861
nick black <dankamongmen@gmail.com>
2020-04-01 06:56:34 -0400

    Throw up an ncplot unit test

---
+++ tests/plot.cpp
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <cstring>
+#include <iostream>
+
+TEST_CASE("Plot") {
+  if(!enforce_utf8()){
+    return;
+  }
+  if(getenv("TERM") == nullptr){
+    return;
+  }
+  notcurses_options nopts{};
+  nopts.inhibit_alternate_screen = true;
+  nopts.suppress_banner = true;
+  FILE* outfp_ = fopen("/dev/tty", "wb");
+  REQUIRE(outfp_);
+  struct notcurses* nc_ = notcurses_init(&nopts, outfp_);
+  REQUIRE(nc_);
+  struct ncplane* n_ = notcurses_stdplane(nc_);
+  REQUIRE(n_);
+  REQUIRE(0 == ncplane_cursor_move_yx(n_, 0, 0));
+
+  SUBCASE("SimplePlot"){
+    ncplot_options popts{};
+    ncplot* p = ncplot_create(n_, &popts);
+    REQUIRE(p);
+    CHECK(n_ == ncplot_plane(p));
+    ncplot_destroy(p);
+  }
+
+  CHECK(0 == notcurses_stop(nc_));
+  CHECK(0 == fclose(outfp_));
+}
