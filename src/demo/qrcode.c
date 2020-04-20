e7811cd828fc0e5d57063072603645dcff2e547e
nick black <dankamongmen@gmail.com>
2020-04-20 08:03:08 -0400

    add some flash to the qrcode demo #24

--- src/demo/qrcode.c
+++ src/demo/qrcode.c
@@ -4,7 +4,8 @@
 int qrcode_demo(struct notcurses* nc){
 #ifdef USE_QRCODEGEN
   char data[128];
-  struct ncplane* n = notcurses_stdplane(nc);
+  int dimy, dimx;
+  struct ncplane* n = notcurses_stddim_yx(nc, &dimy, &dimx);
   for(int i = 0 ; i < 1024 ; ++i){
     ncplane_erase(n);
     size_t len = random() % sizeof(data) + 1;
@@ -16,6 +17,17 @@ int qrcode_demo(struct notcurses* nc){
       return -1;
     }
     if(ncplane_qrcode(n, 0, data, len) <= 0){
+      return -1;
+    }
+    if(ncplane_cursor_move_yx(n, 0, 0)){
+      return -1;
+    }
+    uint64_t tl = 0, bl = 0, br = 0, tr = 0;
+    channels_set_fg_rgb(&tl, random() % 255 + 1, random() % 255 + 1, random() % 255 + 1);
+    channels_set_fg_rgb(&tr, random() % 255 + 1, random() % 255 + 1, random() % 255 + 1);
+    channels_set_fg_rgb(&bl, random() % 255 + 1, random() % 255 + 1, random() % 255 + 1);
+    channels_set_fg_rgb(&br, random() % 255 + 1, random() % 255 + 1, random() % 255 + 1);
+    if(ncplane_stain(n, dimy - 1, dimx - 1, tl, tr, bl, br) <= 0){
       return -1;
     }
     DEMO_RENDER(nc);
