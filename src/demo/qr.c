3c6c0913029ee83720f797e5a41382b23c95ce0c
nick black <dankamongmen@gmail.com>
2020-04-20 05:00:45 -0400

    qrcode demo #24

--- src/demo/qr.c
+++
@@ -1,7 +0,0 @@
-#include "demo.h"
-
-int qrcode_demo(struct notcurses* nc){
-  // FIXME
-  DEMO_RENDER(nc);
-  return 0;
-}
---
+++ src/demo/qrcode.c
@@ -0,0 +1,23 @@
+#include "demo.h"
+#include <sys/random.h>
+
+int qrcode_demo(struct notcurses* nc){
+#ifdef USE_QRCODEGEN
+  char data[128];
+  struct ncplane* n = notcurses_stdplane(nc);
+  for(int i = 0 ; i < 1024 ; ++i){
+    size_t len = random() % sizeof(data) + 1;
+    ssize_t got = getrandom(data, len, 0);
+    if(got < 0 || (size_t)got != len){
+      return -1;
+    }
+    if(ncplane_qrcode(n, 0, data, len) <= 0){
+      return -1;
+    }
+    DEMO_RENDER(nc);
+  }
+#else
+  DEMO_RENDER(nc);
+#endif
+  return 0;
+}
