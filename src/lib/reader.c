064da8acb985e86a438e656c91c7c9078392541e
nick black <dankamongmen@gmail.com>
2020-05-07 14:32:33 -0400

    ncreader_destroy() / ncreader_plane()

--- src/lib/reader.c
+++ src/lib/reader.c
@@ -1,14 +1,21 @@
 #include "internal.h"
 
-ncreader* ncreader_create(ncplane* n){
+ncreader* ncreader_create(int rows, int cols, int y, int x){
+  // FIXME
 }
 
 // empty the ncreader of any user input, and home the cursor.
 int ncreader_clear(ncreader* n){
+  // FIXME
 }
 
 ncplane* ncreader_plane(ncreader* n){
+  return n->ncp;
 }
 
 void ncreader_destroy(ncreader* n){
+  if(n){
+    ncplane_destroy(n->ncp);
+    free(n);
+  }
 }
