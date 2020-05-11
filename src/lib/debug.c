454806d53a70f1d55eb9f7a21ab313cfb647d804
nick black <dankamongmen@gmail.com>
2020-05-10 20:23:02 -0400

    extend notcurses_debug()

--- src/lib/debug.c
+++ src/lib/debug.c
@@ -5,9 +5,11 @@ void notcurses_debug(notcurses* nc, FILE* debugfp){
   int planeidx = 0;
   fprintf(debugfp, "********************* notcurses debug state *********************\n");
   while(n){
-    fprintf(stderr, "%02d] abs: %d/%d len: %d/%d cursor: %d/%d %p\n", d,
-            p->absy, p->absx, p->leny, p->lenx, p->y, p->x, p);
+   fprintf(debugfp, "%04d off y: %3d x: %3d geom y: %3d x: %3d curs y: %3d x: %3d %s %p\n",
+           planeidx, n->absy, n->absx, n->leny, n->lenx, n->y, n->x,
+           n == notcurses_stdplane_const(nc) ? "std" : "   ", n);
     n = n->z;
+    ++planeidx;
   }
   fprintf(debugfp, "*****************************************************************\n");
 }
