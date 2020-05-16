e751557936ebd267c3ecb28b92ecf11819ea5c79
nick black <dankamongmen@gmail.com>
2020-05-16 18:48:08 -0400

    debug(): stretch out headers/footers

--- src/lib/debug.c
+++ src/lib/debug.c
@@ -3,7 +3,7 @@
 void notcurses_debug(notcurses* nc, FILE* debugfp){
   const ncplane* n = nc->top;
   int planeidx = 0;
-  fprintf(debugfp, "********************* notcurses debug state *********************\n");
+  fprintf(debugfp, "*************************** notcurses debug state *****************************\n");
   while(n){
    fprintf(debugfp, "%04d off y: %3d x: %3d geom y: %3d x: %3d curs y: %3d x: %3d %s %p\n",
            planeidx, n->absy, n->absx, n->leny, n->lenx, n->y, n->x,
@@ -11,5 +11,5 @@ void notcurses_debug(notcurses* nc, FILE* debugfp){
     n = n->z;
     ++planeidx;
   }
-  fprintf(debugfp, "*****************************************************************\n");
+  fprintf(debugfp, "*******************************************************************************\n");
 }
