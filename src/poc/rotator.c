e18bfe9a31d28b5c0db8aa2d0e9bd47186365b2a
nick black <dankamongmen@gmail.com>
2020-06-04 19:52:23 -0400

    rotator poc: rotate a fullplane gradient #672

--- src/poc/rotator.c
+++ src/poc/rotator.c
@@ -5,6 +5,50 @@
 #include <notcurses/notcurses.h>
 
 static int
+rotate_grad(struct notcurses* nc){
+  struct timespec ts = {
+    .tv_sec = 0,
+    .tv_nsec = 750000000,
+  };
+  int dimy, dimx;
+  struct ncplane* n = notcurses_stddim_yx(nc, &dimy, &dimx);
+  ncplane_cursor_move_yx(n, 0, 0);
+  uint64_t tl = 0, tr = 0, bl = 0, br = 0;
+  channels_set_fg_rgb(&tl, 0, 0, 0);
+  channels_set_fg_rgb(&tr, 0, 0, 0xff);
+  channels_set_fg_rgb(&bl, 0, 0xff, 0);
+  channels_set_fg_rgb(&br, 0, 0xff, 0xff);
+  channels_set_bg_rgb(&tl, 0, 0, 0);
+  channels_set_bg_rgb(&tr, 0, 0xff, 0);
+  channels_set_bg_rgb(&bl, 0, 0, 0xff);
+  channels_set_bg_rgb(&br, 0, 0xff, 0xff);
+  //if(ncplane_gradient(n, "a", 0, tl, tr, bl, br, dimy - 1, dimx - 1) <= 0){
+  if(ncplane_highgradient(n, tl, tr, bl, br, dimy - 1, dimx - 1) <= 0){
+    return -1;
+  }
+  notcurses_render(nc);
+  clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);
+  uint32_t* rgba = ncplane_rgba(n, 0, 0, dimy, dimx);
+  if(rgba == NULL){
+    return -1;
+  }
+  ncplane_erase(n);
+  notcurses_render(nc);
+  clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);;
+
+  if(ncplane_blit_rgba(n, 0, 0, dimx * 4, NCBLIT_DEFAULT,
+                       rgba, 0, 0, dimy * 2, dimx) < 0){
+    free(rgba);
+    return -1;
+  }
+  free(rgba);
+  notcurses_render(nc);
+  clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);;
+
+  return 0;
+}
+
+static int
 rotate(struct notcurses* nc){
   struct timespec ts = {
     .tv_sec = 0,
@@ -80,6 +124,7 @@ rotate(struct notcurses* nc){
 
   // now promote it to a visual
   struct ncvisual* v = ncvisual_from_rgba(rgba, 4, XSIZE * 4, XSIZE);
+  free(rgba);
   if(v == NULL){
     return -1;
   }
@@ -92,21 +137,27 @@ rotate(struct notcurses* nc){
   notcurses_render(nc);
   clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);;
 
-  if(NCERR_SUCCESS != ncvisual_rotate(v, M_PI / 2)){
-    return -1;
+  for(int i = 0 ; i < 4 ; ++i){
+    if(NCERR_SUCCESS != ncvisual_rotate(v, M_PI / 2)){
+      return -1;
+    }
+    ncplane_erase(n);
+    ncvisual_render(nc, v, &vopts);
+    notcurses_render(nc);
+    clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);;
   }
-  ncplane_erase(n);
-  ncvisual_render(nc, v, &vopts);
-  notcurses_render(nc);
-  clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);;
 
-  if(NCERR_SUCCESS != ncvisual_rotate(v, M_PI / 4)){
-    return -1;
+  for(int i = 0 ; i < 8 ; ++i){
+    if(NCERR_SUCCESS != ncvisual_rotate(v, M_PI / 4)){
+      return -1;
+    }
+    ncplane_erase(n);
+    ncvisual_render(nc, v, &vopts);
+    notcurses_render(nc);
+    clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);;
   }
-  ncplane_erase(n);
-  ncvisual_render(nc, v, &vopts);
-  notcurses_render(nc);
-  clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);;
+
+  ncvisual_destroy(v);
   return 0;
 }
 
@@ -117,7 +168,9 @@ int main(void){
     .flags = NCOPTION_INHIBIT_SETLOCALE,
   };
   struct notcurses* nc = notcurses_init(&nopts, NULL);
-  int r = rotate(nc);
+  int r = 0;
+  r |= rotate(nc);
+  r |= rotate_grad(nc);
   r |= notcurses_stop(nc);
   return r ? EXIT_FAILURE : EXIT_SUCCESS;
 }
