ddb0db3adba0fc65cfa230d99dcd6705a797ff44
nick black <dankamongmen@gmail.com>
2020-06-12 12:07:47 -0400

    stash

--- src/demo/dragon.c
+++ src/demo/dragon.c
@@ -1,5 +1,56 @@
 #include "demo.h"
 
+static void
+set_colors(uint64_t* tl, uint64_t* tr, uint64_t* bl, uint64_t* br){
+  channels_set_fg_rgb(tl, random() % 256, random() % 256, random() % 256);
+  channels_set_bg_rgb(tl, 0, 0, 0);
+  *tr = *tl;
+  *bl = *tl;
+  *br = *tl;
+}
+
+// start with a line. on each iteration, rotate a copy of what we have 90deg
+// cw, and attach it to the end of what we drew. very easy in notcurses!
 int dragon_demo(struct notcurses* nc){
+  const int ITERATIONS = 10;
+  int dimy, dimx;
+  struct ncplane* n = notcurses_stddim_yx(nc, &dimy, &dimx);
+  cell c = CELL_TRIVIAL_INITIALIZER;
+  if(cell_load(n, &c, "█") <= 0){
+    return -1;
+  }
+  cell_set_fg_rgb(&c, 0, 0xff, 0);
+  ncplane_cursor_move_yx(n, dimy / 2, dimx / 2);
+  if(ncplane_vline(n, &c, 2) < 2){
+    cell_release(n, &c);
+    return -1;
+  }
+  cell_release(n, &c);
+  DEMO_RENDER(nc);
+  demo_nanosleep(nc, &demodelay);
+  for(int iter = 0 ; iter < ITERATIONS ; ++iter){
+    struct ncplane* newn = ncplane_dup(n, n);
+    uint64_t channels = 0;
+    channels_set_fg_alpha(&channels, CELL_ALPHA_TRANSPARENT);
+    channels_set_bg_alpha(&channels, CELL_ALPHA_TRANSPARENT);
+    ncplane_set_base(newn, " ", 0, channels);
+    if(NULL == newn){
+      return -1;
+    }
+    uint64_t tl = 0, tr = 0, bl = 0, br = 0;
+    set_colors(&tl, &tr, &bl, &br);
+    ncplane_dim_yx(newn, &dimy, &dimx);
+    ncplane_cursor_move_yx(newn, 0, 0);
+    if(ncplane_stain(newn, dimy - 1, dimx - 1, tl, tr, bl, br) < 0){
+      return -1;
+    }
+    if(ncplane_rotate_cw(newn)){
+      return -1;
+    }
+    DEMO_RENDER(nc);
+    demo_nanosleep(nc, &demodelay);
+    n = newn;
+    notcurses_debug(nc, stderr);
+  }
   return 0;
 }
