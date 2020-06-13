78ff3f7422307e3d33d49010eab2f0c42019ae42
nick black <dankamongmen@gmail.com>
2020-06-12 23:48:14 -0400

    dragon: ncvisual-based #708

--- src/demo/dragon.c
+++ src/demo/dragon.c
@@ -2,24 +2,25 @@
 
 // lame as fuck lol
 static bool done;
+static uint32_t pixel;
 static int y, x, dy, dx;
 
 static int
-dragonmayer(struct ncplane* n, const char* str, int iters){
+dragonmayer(struct ncvisual* ncv, const char* str, int iters){
   char c;
   int r;
   while( (c = *str++) ){
     switch(c){
       case 'X':
         if(iters > 1){
-          if( (r = dragonmayer(n, "X+YF+", iters - 1)) ){
+          if( (r = dragonmayer(ncv, "X+YF+", iters - 1)) ){
             return r;
           }
         }
         break;
       case 'Y':
         if(iters > 1){
-          if( (r = dragonmayer(n, "-FX-Y", iters - 1)) ){
+          if( (r = dragonmayer(ncv, "-FX-Y", iters - 1)) ){
             return r;
           }
         }
@@ -28,7 +29,7 @@ dragonmayer(struct ncplane* n, const char* str, int iters){
       case '-': { int tmp = -dy; dy = dx; dx = tmp; break; }
       case 'F': // FIXME want a line
         // FIXME some of these will fail...hella lame, check against dims
-        if(ncplane_putsimple_yx(n, y, x, '@') <= 0){
+        if(ncvisual_set_yx(ncv, y, x, pixel) < 0){
           done = true;
         }
         x += dx;
@@ -49,6 +50,8 @@ int dragon_demo(struct notcurses* nc){
   // + is turn right.
   const char LINDENSTART[] = "FX";
   const int SCALE = 1;
+  dimy *= 2;
+  dimx *= 2;
   int dxstart, dystart;
   if(dimy > dimx){
     dystart = 0;
@@ -57,25 +60,49 @@ int dragon_demo(struct notcurses* nc){
     dystart = SCALE;
     dxstart = 0;
   }
+  size_t fbbytes = sizeof(uint32_t) * dimy * dimx;
+  uint32_t* rgba = malloc(fbbytes);
+  if(rgba == NULL){
+    return -1;
+  }
+  memset(rgba, 0, fbbytes);
+  for(int i = 0 ; i < dimy * dimx ; ++i){
+    ncpixel_set_a(&rgba[i], 0xff);
+  }
+  struct ncvisual* ncv = ncvisual_from_rgba(rgba, dimy, dimx * sizeof(uint32_t), dimx);
+  if(ncv == NULL){
+    free(rgba);
+    return -1;
+  }
+  free(rgba);
   struct timespec scaled;
   timespec_div(&demodelay, 4, &scaled);
   int iters = 0;
   do{
     ++iters;
-    if(ncplane_set_fg_rgb(n, 0, 0x11 * iters, 0)){
-      break;
-    }
+    pixel = 0xffffffffull;
+    ncpixel_set_rgb(&pixel, 0, 0x11 * iters, 0);
     dx = dxstart;
     dy = dystart;
     x = dimx / 2;
     y = dimy / 2;
-    int r = dragonmayer(n, LINDENSTART, iters);
+    int r = dragonmayer(ncv, LINDENSTART, iters);
     if(r){
+      ncvisual_destroy(ncv);
       return r;
+    }
+    struct ncvisual_options vopts = {
+      .n = n,
+      .blitter = NCBLIT_2x2,
+    };
+    if(ncvisual_render(nc, ncv, &vopts) == NULL){
+      ncvisual_destroy(ncv);
+      return -1;
     }
     DEMO_RENDER(nc);
     demo_nanosleep(nc, &scaled);
     ncplane_erase(n);
   }while(!done);
+  ncvisual_destroy(ncv);
   return 0;
 }
