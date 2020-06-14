bea281d120281e5ae7d0ffd98fa99c7da8e0edbb
nick black <dankamongmen@gmail.com>
2020-06-14 18:16:44 -0400

    rust: update for new notcurses_options

--- rust/notcurses/src/lib.rs
+++ rust/notcurses/src/lib.rs
@@ -48,18 +48,14 @@ mod tests {
         unsafe {
             let _ = libc::setlocale(libc::LC_ALL, std::ffi::CString::new("").unwrap().as_ptr());
             let opts: ffi::notcurses_options = ffi::notcurses_options {
-                inhibit_alternate_screen: false,
                 loglevel: 0,
                 termtype: std::ptr::null(),
-                retain_cursor: false,
-                suppress_banner: false,
-                no_winch_sighandler: false,
-                no_quit_sighandlers: false,
                 renderfp: std::ptr::null_mut(),
                 margin_t: 0,
                 margin_r: 0,
                 margin_b: 0,
                 margin_l: 0,
+                flags: ffi::NCOPTION_NO_ALTERNATE_SCREEN as u64,
             };
             let nc = ffi::notcurses_init(&opts, stdout);
             assert_ne!(std::ptr::null(), nc);
