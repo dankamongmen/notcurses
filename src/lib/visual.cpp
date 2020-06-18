29fe68f93adebd8da8cc8b24d26fd117651b6001
nick black <dankamongmen@gmail.com>
2020-06-18 03:40:30 -0400

    ncvisual_geom(): handle NULL vopts case #727

--- src/lib/visual.cpp
+++ src/lib/visual.cpp
@@ -67,19 +67,19 @@ auto ncvisual_geom(const notcurses* nc, const ncvisual* n,
     x = &fauxx;
   }
   if(n){
-    if(vopts->scaling == NCSCALE_NONE){
+    if(!vopts || vopts->scaling == NCSCALE_NONE){
       *y = n->rows;
     }else{
       int rows = vopts->n ? ncplane_dim_y(vopts->n) : ncplane_dim_y(nc->stdscr);
       *y = rows * encoding_y_scale(bset);
     }
-    if(vopts->scaling == NCSCALE_NONE){
+    if(!vopts || vopts->scaling == NCSCALE_NONE){
       *x = n->cols;
     }else{
       int cols = vopts->n ? ncplane_dim_x(vopts->n) : ncplane_dim_x(nc->stdscr);
       *x = cols * encoding_x_scale(bset);
     }
-    if(vopts->scaling == NCSCALE_SCALE){
+    if(vopts && vopts->scaling == NCSCALE_SCALE){
       scale_visual(n, y, x);
     }
   }
