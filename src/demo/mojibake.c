0d4942417e6f2668faa034287b339d8df175ae64
nick black <dankamongmen@gmail.com>
2020-06-21 00:38:22 -0400

    mojibake title

--- src/demo/mojibake.c
+++ src/demo/mojibake.c
@@ -4,5 +4,13 @@ int mojibake_demo(struct notcurses* nc){
   if(!notcurses_canutf8(nc)){
     return 0;
   }
+  int dimy, dimx;
+  struct ncplane* std = notcurses_stddim_yx(nc, &dimy, &dimx);
+  ncplane_set_fg(std, 0xffffff);
+  if(ncplane_putstr_aligned(std, 4, NCALIGN_CENTER, "mojibake 文字化けmodʑibake") < 0){
+    return -1;
+  }
+  DEMO_RENDER(nc);
+  demo_nanosleep(nc, &demodelay);
   return 0;
 }
