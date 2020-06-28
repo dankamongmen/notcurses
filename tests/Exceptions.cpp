dd2b36ce0a951b4cd9145fe61028d51550aed940
nick black <dankamongmen@gmail.com>
2020-06-28 07:21:22 -0400

    drone: set TERM on debian

--- tests/Exceptions.cpp
+++ tests/Exceptions.cpp
@@ -3,26 +3,22 @@
 
 using namespace ncpp;
 
-TEST_CASE("Exceptions") {
+TEST_CASE("ExceptionsGetInstance") {
+  CHECK_THROWS_AS(NotCurses::get_instance(), invalid_state_error);
+}
 
-  SUBCASE("GetInstance") {
-    CHECK_THROWS_AS(NotCurses::get_instance(), invalid_state_error);
-  }
-
-  SUBCASE("ResetStats") {
-    NotCurses nc;
-    // FIXME attempts to match invalid_argument have failed thus far :/
-    CHECK_THROWS(nc.reset_stats(nullptr));
-    CHECK(nc.stop());
-  }
+TEST_CASE("ResetStats") {
+  NotCurses nc;
+  // FIXME attempts to match invalid_argument have failed thus far :/
+  CHECK_THROWS(nc.reset_stats(nullptr));
+  CHECK(nc.stop());
+}
 
   // ncpp only allows one notcurses object at a time (why?)
-  SUBCASE("OnlyOneNotCurses") {
-    NotCurses nc;
-    std::unique_ptr<NotCurses> nc2;
-    // FIXME attempts to match ::init_error have failed thus far :/
-    CHECK_THROWS(nc2.reset(new NotCurses()));
-    CHECK(nc.stop());
-  }
-
+TEST_CASE("OnlyOneNotCurses") {
+  NotCurses nc;
+  std::unique_ptr<NotCurses> nc2;
+  // FIXME attempts to match ::init_error have failed thus far :/
+  CHECK_THROWS(nc2.reset(new NotCurses()));
+  CHECK(nc.stop());
 }
