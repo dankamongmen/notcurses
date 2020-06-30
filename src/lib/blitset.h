3bb789eb74a8e68113f46250d28761f9e96ed06f
nick black <dankamongmen@gmail.com>
2020-06-29 23:03:58 -0400

    refactoring to prep for ncdirect_visual

--- src/lib/blitset.h
+++ src/lib/blitset.h
@@ -27,38 +27,6 @@ lookup_blitset(bool utf8, ncblitter_e setid, bool may_degrade) {
   return NULL;
 }
 
-static inline ncblitter_e
-ncvisual_default_blitter(bool canutf8) {
-  if(canutf8){
-    // FIXME probably want 2x2, at least for images larger than plane
-    return NCBLIT_2x1;
-  }
-  return NCBLIT_1x1;
-}
-
-// RGBA visuals all use NCBLIT_2x1 by default (or NCBLIT_1x1 if not in
-// UTF-8 mode), but an alternative can be specified.
-static inline const struct blitset*
-rgba_blitter(bool utf8, ncblitter_e setid, bool maydegrade) {
-  const struct blitset* bset;
-  if(setid != NCBLIT_DEFAULT){
-    bset = lookup_blitset(utf8, setid, maydegrade);
-  }else{
-    bset = lookup_blitset(utf8, ncvisual_default_blitter(utf8), maydegrade);
-  }
-  if(bset && !bset->blit){ // FIXME remove this once all blitters are enabled
-    bset = NULL;
-  }
-  return bset;
-}
-
-// convenience wrapper around rgba_blitter()
-static inline const struct blitset*
-ncv_rgba_blitter(bool utf8, const struct ncvisual_options* opts) {
-  const bool maydegrade = !(opts && (opts->flags & NCVISUAL_OPTION_NODEGRADE));
-  return rgba_blitter(utf8, opts ? opts->blitter : NCBLIT_DEFAULT, maydegrade);
-}
-
 // number of pixels that map to a single cell, height-wise
 static inline int
 encoding_y_scale(const struct blitset* bset) {
@@ -69,6 +37,44 @@ encoding_y_scale(const struct blitset* bset) {
 static inline int
 encoding_x_scale(const struct blitset* bset) {
   return bset->width;
+}
+
+static inline ncblitter_e
+ncvisual_default_blitter(bool utf8, ncscale_e scale) {
+  if(utf8){
+    // NCBLIT_2x2 is better image quality, especially for large images, but
+    // it's not the general default because it doesn't preserve aspect ratio.
+    // NCSCALE_STRETCH throws away aspect ratio, and can safely use NCBLIT_2x2.
+    if(scale == NCSCALE_STRETCH){
+      return NCBLIT_2x2;
+    }
+    return NCBLIT_2x1;
+  }
+  return NCBLIT_1x1;
+}
+
+static inline const struct blitset*
+rgba_blitter_low(bool utf8, ncscale_e scale, bool maydegrade, ncblitter_e blitrec) {
+  const struct blitset* bset;
+  if(blitrec != NCBLIT_DEFAULT){
+    bset = lookup_blitset(utf8, blitrec, maydegrade);
+  }else{
+    bset = lookup_blitset(utf8, ncvisual_default_blitter(utf8, scale), maydegrade);
+  }
+  if(bset && !bset->blit){ // FIXME remove this once all blitters are enabled
+    bset = NULL;
+  }
+  return bset;
+}
+
+// RGBA visuals all use NCBLIT_2x1 by default (or NCBLIT_1x1 if not in
+// UTF-8 mode), but an alternative can be specified.
+static inline const struct blitset*
+rgba_blitter(const struct notcurses* nc, const struct ncvisual_options* opts) {
+  const bool maydegrade = !(opts && (opts->flags & NCVISUAL_OPTION_NODEGRADE));
+  const ncscale_e scale = opts ? opts->scaling : NCSCALE_NONE;
+  return rgba_blitter_low(notcurses_canutf8(nc), scale, maydegrade,
+                          opts ? opts->blitter : NCBLIT_DEFAULT);
 }
 
 #endif
