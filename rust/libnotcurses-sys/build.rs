e3e7b40440f9233b2592e50f590d60a720ddb6be
Nick Black <dankamongmen@gmail.com>
2020-07-13 02:27:02 -0400

    rust: use pkg-config results for -I values #788

--- rust/libnotcurses-sys/build.rs
+++ rust/libnotcurses-sys/build.rs
@@ -1,12 +1,13 @@
 extern crate bindgen;
+extern crate pkg_config;
 
 use std::env;
 use std::path::PathBuf;
 
 // largely taken from https://rust-lang.github.io/rust-bindgen/tutorial-3.html
 fn main() {
-    pkg_config::Config::new()
-        .atleast_version("1.4.2")
+    let plib = pkg_config::Config::new()
+        .atleast_version("1.6.1")
         .probe("notcurses")
         .unwrap();
 
@@ -17,19 +18,23 @@ fn main() {
         .file("stdout.c")
         .compile("stdout");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
-        .clang_arg("-I../../include -D_XOPEN_SOURCE") // FIXME pass via envvar?
+    // The bindgen::Builder is the main entry point to bindgen, and lets you
+    // build up options for the resulting bindings.
+    let mut builder = bindgen::Builder::default()
+        .clang_arg("-D_XOPEN_SOURCE")
         // The input header we would like to generate
-        // bindings for.
+        // builder for.
         .header("wrapper.h")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
-        .generate()
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks));
+
+    for d in plib.include_paths {
+        builder = builder.clang_arg(format!("-I{}", d.to_string_lossy()));
+    }
+
+    // Finish the builder and generate the builder.
+    let bindings = builder.generate()
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
