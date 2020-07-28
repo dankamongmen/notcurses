988bb1f2a621c32ba2799705f6d2a539ae69cbf3
nick black <dankamongmen@gmail.com>
2020-07-28 14:48:51 -0400

    zoo: create selector

--- src/demo/zoo.c
+++ src/demo/zoo.c
@@ -1,8 +1,20 @@
 #include "demo.h"
 
 int zoo_demo(struct notcurses* nc){
+  struct ncplane* n = notcurses_stdplane(nc);
+  ncselector_options sopts = {
+    .maxdisplay = 4,
+    .title = "single-item selector",
+  };
+  struct ncselector* selector = ncselector_create(n, 1, 1, &sopts);
+  if(selector == NULL){
+    return -1;
+  }
+  DEMO_RENDER(nc);
   // FIXME swoop a selector in from the right
+  demo_nanosleep(nc, &demodelay);
+  ncselector_destroy(selector, NULL);
   // FIXME swoop a multiselector in from the left
-  (void)nc; // FIXME
+  DEMO_RENDER(nc);
   return 0;
 }
