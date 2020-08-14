2ceb2f50975c1c475bb64c45453ff47273666b42
nick black <dankamongmen@gmail.com>
2020-08-14 04:57:53 -0400

    egcpool_extended_gcluster: assert() for great justice

--- src/lib/egcpool.h
+++ src/lib/egcpool.h
@@ -255,6 +255,7 @@ cell_egc_idx(const cell* c){
 // only applies to complex cells, do not use on simple cells
 __attribute__ ((__returns_nonnull__)) static inline const char*
 egcpool_extended_gcluster(const egcpool* pool, const cell* c) {
+  assert(!cell_simple_p(c));
   uint32_t idx = cell_egc_idx(c);
   return pool->pool + idx;
 }
