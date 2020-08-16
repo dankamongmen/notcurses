ddcb5ca60c0b48f37c42ff08b6f8f75de96f7902
nick black <dankamongmen@gmail.com>
2020-08-16 08:55:38 -0400

    release.sh: only one level of rust

--- tools/release.sh
+++ tools/release.sh
@@ -51,12 +51,12 @@ echo "The bastards are trying to immanentize the Eschaton"
 # requires token in ~/.netrc
 github-release dankamongmen/notcurses create v$VERSION --name "v$VERSION—$QUIP" --publish $TARBALL.asc
 
-cd ../../python
+cd ../python
 python3 setup.py sdist
 twine upload -s -udankamongmen dist/*
 
 # FIXME need to install fresh notcurses for rust to compile
-cd rust
+cd ../rust
 cargo clean
 cargo publish
 # FIXME and at this point, uninstall, yuck
