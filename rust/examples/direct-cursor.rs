a09ed747ca3d9d903251378c39528311ce284eb4
joseLuís <joseluis@andamira.net>
2020-09-21 20:03:18 +0200

    rust: update example direct-cursor

--- rust/examples/direct-cursor.rs
+++ rust/examples/direct-cursor.rs
@@ -9,19 +9,34 @@ fn main() {
     unsafe {
         let ncd = nc::ncdirect_start();
 
+        let cols = nc::ncdirect_dim_x(ncd);
+        let rows = nc::ncdirect_dim_y(ncd);
+        println!("terminal size (rows, cols): {}, {}", rows, cols);
+
+        // show current coordinates
         let (mut cy, mut cx) = (0,0);
         nc::ncdirect_cursor_yx(ncd, &mut cy, &mut cx);
         nc::ncdirect_putstr(ncd, 0, CString::new(format!("({},{})\n", cy, cx)).unwrap().as_ptr());
 
-        // ISSUE 1: printing without a newline doesn't update the screen
-        sleep(Duration::new(2, 0));
+        // Write HELLO WORLD in steps
+
+        sleep(Duration::new(1, 0));
+
         nc::ncdirect_putstr(ncd, 0, CString::new("HELLO").unwrap().as_ptr());
-        sleep(Duration::new(2, 0));
-        nc::ncdirect_putstr(ncd, 0, CString::new("WORLD").unwrap().as_ptr());
 
+        nc::ncdirect_cursor_enable(ncd); // force flush
+        sleep(Duration::new(1, 0));
+
+        nc::ncdirect_putstr(ncd, 0, CString::new(" WORLD").unwrap().as_ptr());
+
+        nc::ncdirect_cursor_enable(ncd); // force flush
+        sleep(Duration::new(1, 0));
+
+        // show current coordinates
         nc::ncdirect_cursor_yx(ncd, &mut cy, &mut cx);
-        nc::ncdirect_putstr(ncd, 0, CString::new(format!("({},{})\n", cy, cx)).unwrap().as_ptr());
+        nc::ncdirect_putstr(ncd, 0, CString::new(format!(" ({},{})\n", cy, cx)).unwrap().as_ptr());
 
+        sleep(Duration::new(1, 0));
         nc::ncdirect_stop(ncd);
     }
 }
