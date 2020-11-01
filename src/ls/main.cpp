3c26cdcbae105f879fcdfc342e20fae6d805ed7d
nick black <dankamongmen@gmail.com>
2020-11-01 07:21:18 -0500

    ncls: more command line parameters

--- src/ls/main.cpp
+++ src/ls/main.cpp
@@ -5,16 +5,31 @@
 
 static void
 usage(std::ostream& os, const char* name, int code){
-  os << "usage: " << name << " -h | paths...\n";
+  os << "usage: " << name << " -h | [ -lR ] paths...\n";
+  os << " -d: list directories themselves, not their contents\n";
+  os << " -l: use a long listing format\n";
+  os << " -R: list subdirectories recursively\n";
   os << " -h: print usage information\n";
   os << std::flush;
   exit(code);
 }
 
 int main(int argc, char** argv){
+  bool longlisting = false;
+  bool recursedirs = false;
+  bool directories = false;
   int c;
-  while((c = getopt(argc, argv, "h")) != -1){
+  while((c = getopt(argc, argv, "dhlR")) != -1){
     switch(c){
+      case 'd':
+        directories = true;
+        break;
+      case 'l':
+        longlisting = true;
+        break;
+      case 'R':
+        recursedirs = true;
+        break;
       case 'h':
         usage(std::cout, argv[0], EXIT_SUCCESS);
         break;
@@ -23,6 +38,7 @@ int main(int argc, char** argv){
         break;
     }
   }
+  // FIXME if argv[optind] == nullptr, pass "."
   while(argv[optind]){
     std::cout << "arg: " << argv[optind] << std::endl;
     ++optind;
