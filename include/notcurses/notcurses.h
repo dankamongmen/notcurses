dd8423dff366769015510dd1cf918547837502a3
nick black <dankamongmen@gmail.com>
2020-11-28 17:08:45 -0500

    embiggen notcurses_metric(3), add unit test #1107

--- include/notcurses/notcurses.h
+++ include/notcurses/notcurses.h
@@ -2608,6 +2608,30 @@ API struct ncplane* nctablet_plane(struct nctablet* t);
 API struct ncplane* nctablet_ncplane(struct nctablet* t)
   __attribute__ ((deprecated));
 
+// Takes an arbitrarily large number, and prints it into a fixed-size buffer by
+// adding the necessary SI suffix. Usually, pass a |[IB]PREFIXSTRLEN+1|-sized
+// buffer to generate up to |[IB]PREFIXCOLUMNS| columns' worth of EGCs. The
+// characteristic can occupy up through |mult-1| characters (3 for 1000, 4 for
+// 1024). The mantissa can occupy either zero or two characters.
+//
+// Floating-point is never used, because an IEEE758 double can only losslessly
+// represent integers through 2^53-1.
+//
+// 2^64-1 is 18446744073709551615, 18.45E(xa). KMGTPEZY thus suffice to handle
+// an 89-bit uintmax_t. Beyond Z(etta) and Y(otta) lie lands unspecified by SI.
+// 2^-63 is 0.000000000000000000108, 1.08a(tto).
+// val: value to print
+// decimal: scaling. '1' if none has taken place.
+// buf: buffer in which string will be generated
+// omitdec: inhibit printing of all-0 decimal portions
+// mult: base of suffix system (almost always 1000 or 1024)
+// uprefix: character to print following suffix ('i' for kibibytes basically).
+//   only printed if suffix is actually printed (input >= mult).
+//
+// You are encouraged to consult notcurses_metric(3).
+API const char* ncmetric(uintmax_t val, uintmax_t decimal, char* buf,
+                         int omitdec, uintmax_t mult, int uprefix);
+
 // The number of columns is one fewer, as the STRLEN expressions must leave
 // an extra byte open in case 'µ' (U+00B5, 0xC2 0xB5) shows up. PREFIXCOLUMNS
 // is the maximum number of columns used by a mult == 1000 (standard)
@@ -2629,29 +2653,7 @@ API struct ncplane* nctablet_ncplane(struct nctablet* t)
 #define IPREFIXFMT(x) NCMETRIXFWIDTH((x), IPREFIXCOLUMNS), (x)
 #define BPREFIXFMT(x) NCMETRICFWIDTH((x), BPREFIXCOLUMNS), (x)
 
-// Takes an arbitrarily large number, and prints it into a fixed-size buffer by
-// adding the necessary SI suffix. Usually, pass a |[IB]PREFIXSTRLEN+1|-sized
-// buffer to generate up to |[IB]PREFIXCOLUMNS| columns' worth of EGCs. The
-// characteristic can occupy up through |mult-1| characters (3 for 1000, 4 for
-// 1024). The mantissa can occupy either zero or two characters.
-//
-// Floating-point is never used, because an IEEE758 double can only losslessly
-// represent integers through 2^53-1.
-//
-// 2^64-1 is 18446744073709551615, 18.45E(xa). KMGTPEZY thus suffice to handle
-// an 89-bit uintmax_t. Beyond Z(etta) and Y(otta) lie lands unspecified by SI.
-// 2^-63 is 0.000000000000000000108, 1.08a(tto).
-// val: value to print
-// decimal: scaling. '1' if none has taken place.
-// buf: buffer in which string will be generated
-// omitdec: inhibit printing of all-0 decimal portions
-// mult: base of suffix system (almost always 1000 or 1024)
-// uprefix: character to print following suffix ('i' for kibibytes basically).
-//   only printed if suffix is actually printed (input >= mult).
-API const char* ncmetric(uintmax_t val, uintmax_t decimal, char* buf,
-                         int omitdec, uintmax_t mult, int uprefix);
-
-// Mega, kilo, gigafoo. Use PREFIXSTRLEN + 1.
+// Mega, kilo, gigafoo. Use PREFIXSTRLEN + 1 and PREFIXCOLUMNS.
 static inline const char*
 qprefix(uintmax_t val, uintmax_t decimal, char* buf, int omitdec){
   return ncmetric(val, decimal, buf, omitdec, 1000, '\0');
@@ -2663,7 +2665,7 @@ iprefix(uintmax_t val, uintmax_t decimal, char* buf, int omitdec){
   return ncmetric(val, decimal, buf, omitdec, 1024, '\0');
 }
 
-// Mibi, kebi, gibibytes. Use BPREFIXSTRLEN + 1.
+// Mibi, kebi, gibibytes. Use BPREFIXSTRLEN + 1 and BPREFIXCOLUMNS.
 static inline const char*
 bprefix(uintmax_t val, uintmax_t decimal, char* buf, int omitdec){
   return ncmetric(val, decimal, buf, omitdec, 1024, 'i');
