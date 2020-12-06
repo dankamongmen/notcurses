afa3cd9c34d1280a84a244221d19268fbbaafcef
joseLuís <joseluis@andamira.net>
2020-12-06 17:47:25 +0100

    publish 2.0.9 docs

---
+++ rustdoc/implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["libnotcurses_sys"] = [{"text":"impl&lt;T:&nbsp;Default&gt; Default for __IncompleteArrayField&lt;T&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ rustdoc/implementors/std/io/trait.Read.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["libnotcurses_sys"] = [{"text":"impl Read for NcFile","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
