61e51043a277f35a0866f696bb4a3d0a248abf82
nick black <dankamongmen@gmail.com>
2020-12-08 06:53:51 -0500

    extend Hebrew unit test #1174

--- tests/bidi.cpp
+++ tests/bidi.cpp
@@ -15,11 +15,17 @@ TEST_CASE("Bidi") {
 
   SUBCASE("Hebrew") {
     ncplane_set_scrolling(n_, true);
-    CHECK(0 < ncplane_putstr(n_, "ט כשתקע המלאך החמישי בשופרו ראיתי כוכב נופל ארצה מן מהשמים, וניתן לו מפתח התהום"));
+    CHECK(0 < ncplane_putstr(n_, "המלאך"));
     uint16_t stylemask;
     uint64_t channels;
     auto egc = ncplane_at_yx(n_, 0, 0, &stylemask, &channels);
-    CHECK(0 == strcmp(egc, "ט"));
+    REQUIRE(nullptr != egc);
+    CHECK(0 == strcmp(egc, "ה"));
+    free(egc);
+    egc = ncplane_at_yx(n_, 0, 4, &stylemask, &channels);
+    REQUIRE(nullptr != egc);
+    CHECK(0 == strcmp(egc, "ך"));
+    free(egc);
     // FIXME yes, we place this at 0,0 in our internal view...and yet it
     // is actually placed at the rightmost cell :/
     CHECK(0 == notcurses_render(nc_));
