b99e6b8bc6bbc15c9f225b53f6d6b5b4cef2085a
nick black <dankamongmen@gmail.com>
2020-12-09 01:16:46 +0000

    Visual tests: complete endianness fixes #1130

--- tests/visual.cpp
+++ tests/visual.cpp
@@ -301,8 +301,8 @@ TEST_CASE("Visual") {
           uint64_t channels;
           char* egc = notcurses_at_yx(nc_, y, x, &stylemask, &channels);
           REQUIRE(nullptr != egc);
-          CHECK((rgba[y * 2 * DIMX + x] & 0xffffff) == channels_bg_rgb(channels));
-          CHECK((rgba[(y * 2 + 1) * DIMX + x] & 0xffffff) == channels_fg_rgb(channels));
+          CHECK((htole(rgba[y * 2 * DIMX + x]) & 0xffffff) == channels_bg_rgb(channels));
+          CHECK((htole(rgba[(y * 2 + 1) * DIMX + x]) & 0xffffff) == channels_fg_rgb(channels));
           free(egc);
         }
       }
