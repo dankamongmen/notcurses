82ffac8eedf957ef800232c14d9cb0eff3f8bd45
nick black <dankamongmen@gmail.com>
2020-12-13 07:29:43 -0500

    remove ncprogbar locking options #1202

--- src/lib/progbar.c
+++ src/lib/progbar.c
@@ -13,22 +13,13 @@ ncprogbar* ncprogbar_create(ncplane* n, const ncprogbar_options* opts){
     logerror(ncplane_notcurses(n), "Illegal progbar colors\n");
     return NULL;
   }
-  if(opts->flags > (NCPROGBAR_OPTION_FORCE_VERTICAL << 1u)){
+  if(opts->flags > (NCPROGBAR_OPTION_RETROGRADE << 1u)){
     logwarn(ncplane_notcurses(n), "Invalid flags %016lx\n", opts->flags);
   }
   ncprogbar* ret = malloc(sizeof(*ret));
   ret->ncp = n;
   ret->maxchannels = opts->maxchannels;
   ret->minchannels = opts->minchannels;
-  if(opts->flags & NCPROGBAR_OPTION_LOCK_ORIENTATION){
-    if(opts->flags & NCPROGBAR_OPTION_FORCE_VERTICAL){
-      ret->direction = PROGRESS_VERT;
-    }else{
-      ret->direction = PROGRESS_HORIZ;
-    }
-  }else{
-    ret->direction = PROGRESS_DYNAMIC;
-  }
   ret->retrograde = opts->flags & NCPROGBAR_OPTION_RETROGRADE;
   return ret;
 }
@@ -48,16 +39,10 @@ progbar_redraw(ncprogbar* n){
   // get current dimensions; they might have changed
   int dimy, dimx;
   ncplane_dim_yx(ncprogbar_plane(n), &dimy, &dimx);
-  if(n->direction == PROGRESS_DYNAMIC){
-    if(dimx > dimy){
-      direction = n->retrograde ? DIR_LEFT : DIR_RIGHT;
-    }else{
-      direction = n->retrograde ? DIR_DOWN : DIR_UP;
-    }
-  }else if(n->direction == PROGRESS_VERT){
-    direction = n->retrograde ? DIR_DOWN : DIR_UP;
-  }else{ // PROGRESS_HORIZ
+  if(dimx > dimy){
     direction = n->retrograde ? DIR_LEFT : DIR_RIGHT;
+  }else{
+    direction = n->retrograde ? DIR_DOWN : DIR_UP;
   }
   const bool horizontal = (direction == DIR_LEFT || direction == DIR_RIGHT);
   int delt, range;
