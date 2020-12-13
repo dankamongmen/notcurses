ff798e69e9478e5ddb180826a23d87858555e3bd
nick black <dankamongmen@gmail.com>
2020-12-13 06:58:27 -0500

    progbar PoC: finish up

--- src/poc/progbar.c
+++ src/poc/progbar.c
@@ -25,11 +25,8 @@ pbar_fill(struct notcurses* nc, struct ncprogbar* pbar){
   return 0;
 }
 
-int main(void){
-  struct notcurses* nc = notcurses_init(NULL, NULL);
-  if(nc == NULL){
-    return EXIT_FAILURE;
-  }
+static struct ncprogbar*
+pbar_make(struct notcurses* nc){
   int dimy, dimx;
   struct ncplane* std = notcurses_stddim_yx(nc, &dimy, &dimx);
   struct ncplane_options nopts = {
@@ -42,10 +39,31 @@ int main(void){
   };
   struct ncplane* pbar = ncplane_create(std, &nopts);
   if(pbar == NULL){
-    notcurses_stop(nc);
-    return EXIT_FAILURE;
+    return NULL;
+  }
+  int posy, posx, pdimy, pdimx;
+  ncplane_yx(pbar, &posy, &posx);
+  ncplane_dim_yx(pbar, &pdimy, &pdimx);
+  ncplane_cursor_move_yx(std, posy - 1, posx - 1);
+  uint64_t channels = 0;
+  channels_set_fg_rgb8(&channels, 0, 0xde, 0xde);
+  if(ncplane_rounded_box(std, 0, channels, posy + pdimy, posx + pdimx, 0)){
+    ncplane_destroy(pbar);
+    return NULL;
   }
   struct ncprogbar* ncp = ncprogbar_create(pbar, NULL);
+  if(ncp == NULL){
+    return NULL;
+  }
+  return ncp;
+}
+
+int main(void){
+  struct notcurses* nc = notcurses_init(NULL, NULL);
+  if(nc == NULL){
+    return EXIT_FAILURE;
+  }
+  struct ncprogbar* ncp = pbar_make(nc);
   if(ncp == NULL){
     notcurses_stop(nc);
     return EXIT_FAILURE;
