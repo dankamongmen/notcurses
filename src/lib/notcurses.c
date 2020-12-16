66150c1f42306985ebece1aa34fedbe02a3dd169
nick black <dankamongmen@gmail.com>
2020-12-16 06:04:28 -0500

    ncplane_resize_internal: reject keep > len #1215

--- src/lib/notcurses.c
+++ src/lib/notcurses.c
@@ -591,6 +591,14 @@ int ncplane_resize_internal(ncplane* n, int keepy, int keepx, int keepleny,
   }
   int rows, cols;
   ncplane_dim_yx(n, &rows, &cols);
+  if(keepleny > rows){
+    logerror(ncplane_notcurses(n), "Can't keep %d rows from %d\n", keepleny, rows);
+    return -1;
+  }
+  if(keeplenx > cols){
+    logerror(ncplane_notcurses(n), "Can't keep %d cols from %d\n", keeplenx, cols);
+    return -1;
+  }
   loginfo(ncplane_notcurses(n), "%dx%d @ %d/%d → %d/%d @ %d/%d (keeping %dx%d from %d/%d)\n", rows, cols, n->absy, n->absx, ylen, xlen, n->absy + keepy + yoff, n->absx + keepx + xoff, keepleny, keeplenx, keepy, keepx);
   // we're good to resize. we'll need alloc up a new framebuffer, and copy in
   // those elements we're retaining, zeroing out the rest. alternatively, if
@@ -619,7 +627,7 @@ int ncplane_resize_internal(ncplane* n, int keepy, int keepx, int keepleny,
   // don't use ncplane_move_yx(), because we want to planebinding-invariant.
   n->absy += keepy + yoff;
   n->absx += keepx + xoff;
-fprintf(stderr, "absx: %d keepx: %d xoff: %d\n", n->absx, keepx, xoff);
+//fprintf(stderr, "absx: %d keepx: %d xoff: %d\n", n->absx, keepx, xoff);
   if(keptarea == 0){ // keep nothing, resize/move only
     // if we're keeping nothing, dump the old egcspool. otherwise, we go ahead
     // and keep it. perhaps we ought compact it?
@@ -637,7 +645,7 @@ fprintf(stderr, "absx: %d keepx: %d xoff: %d\n", n->absx, keepx, xoff);
   for(int itery = 0 ; itery < ylen ; ++itery){
     int truey = itery + n->absy;
     int sourceoffy = truey - oldabsy;
-fprintf(stderr, "sourceoffy: %d keepy: %d ylen: %d\n", sourceoffy, keepy, ylen);
+//fprintf(stderr, "sourceoffy: %d keepy: %d ylen: %d\n", sourceoffy, keepy, ylen);
     // if we have nothing copied to this line, zero it out in one go
     if(sourceoffy < keepy || sourceoffy >= keepy + keepleny){
 //fprintf(stderr, "writing 0s to line %d of %d\n", itery, ylen);
@@ -652,7 +660,7 @@ fprintf(stderr, "sourceoffy: %d keepy: %d ylen: %d\n", sourceoffy, keepy, ylen);
         copied += -xoff;
       }
       const int sourceidx = nfbcellidx(n, sourceoffy, keepx);
-fprintf(stderr, "copying line %d (%d) to %d (%d)\n", sourceoffy, sourceidx, copyoff / xlen, copyoff);
+//fprintf(stderr, "copying line %d (%d) to %d (%d)\n", sourceoffy, sourceidx, copyoff / xlen, copyoff);
       memcpy(fb + copyoff, preserved + sourceidx, sizeof(*fb) * keeplenx);
       copyoff += keeplenx;
       copied += keeplenx;
