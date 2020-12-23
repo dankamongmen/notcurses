c07b68f783a7379751dc8cb1535c600fe697e005
joseLuís <joseluis@andamira.net>
2020-12-23 21:50:23 +0100

    rust: add example poc-cjkscroll

---
+++ rust/examples/poc-cjkscroll.rs
@@ -0,0 +1,31 @@
+//! based on the proof of concept at ../../src/poc/cjkscroll.c
+
+use libnotcurses_sys::*;
+
+fn main() {
+
+    let nc = Notcurses::new();
+    let plane = nc.stdplane();
+    plane.set_scrolling(true);
+
+    let mut wc = '\u{4e00}'; // '一';
+
+    loop {
+        sleep![1];
+
+        if plane.putchar(wc) == NCRESULT_ERR {
+            break;
+        }
+
+        wc = core::char::from_u32(wc as u32 + 1).expect("invalid char");
+
+        if wc == '\u{9fa5}' { // 龣
+            wc = '\u{4e00}';
+        }
+
+        if nc.render() == NCRESULT_ERR {
+            break;
+        }
+    }
+    nc.stop();
+}
