b729e655d0bce68c4d7f87ae942f150388672bbf
nick black <dankamongmen@gmail.com>
2021-01-03 19:56:42 -0500

    update rust docs to 2.1.4

---
+++ rustdoc/implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["libnotcurses_sys"] = [{"text":"impl&lt;'a&gt; DerefMut for DirectMode&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; DerefMut for FullMode&lt;'a&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
