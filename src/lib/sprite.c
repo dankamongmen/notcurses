9ffe75f6a076da54b9117724ac256026fea60404
nick black <dankamongmen@gmail.com>
2021-03-15 04:36:30 -0400

    [pixels] build up the sprixel cache #1401

--- src/lib/sprite.c
+++ src/lib/sprite.c
@@ -6,3 +6,22 @@ void sprixel_free(sprixel* s){
     free(s);
   }
 }
+
+sprixel* sprixel_create(ncplane* n, const char* s, int bytes){
+  sprixel* ret = malloc(sizeof(sprixel));
+  if(ret){
+    if((ret->glyph = memdup(s, bytes + 1)) == NULL){
+      free(ret);
+      return NULL;
+    }
+    ret->invalidated = 1;
+    ret->n = n;
+    if(ncplane_pile(n)){
+      notcurses* nc = ncplane_notcurses(n);
+      ret->next = nc->sprixelcache;
+      nc->sprixelcache = ret;
+      ret->id = nc->sprixelnonce++; // FIXME should be atomic
+    }
+  }
+  return ret;
+}
