7b76592e5c17e8e286b48f8133a426786062690c
nick black <dankamongmen@gmail.com>
2021-03-20 03:00:29 -0400

    [pixel] unit test on sprixel_cell_wipe()

--- src/tests/pixel.cpp
+++ src/tests/pixel.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <vector>
 
 TEST_CASE("Pixel") {
   auto nc_ = testing_notcurses();
@@ -9,8 +10,16 @@ TEST_CASE("Pixel") {
   REQUIRE(n_);
 
   if(notcurses_check_pixel_support(nc_) <= 0){
+    CHECK(0 == nc_->tcache.sixel_supported);
     CHECK(!notcurses_stop(nc_));
     return;
+  }
+
+  SUBCASE("SprixelTermValues") {
+    CHECK(0 < nc_->tcache.cellpixy);
+    CHECK(0 < nc_->tcache.cellpixx);
+    CHECK(nc_->tcache.sixel_supported);
+    CHECK(nullptr != nc_->tcache.pixel_cell_wipe);
   }
 
 #ifdef NOTCURSES_USE_MULTIMEDIA
@@ -27,6 +36,40 @@ TEST_CASE("Pixel") {
     ncvisual_destroy(ncv);
   }
 #endif
+
+  SUBCASE("PixelCellWipe") {
+    // first, assemble a visual equivalent to 4 cells
+    auto y = nc_->tcache.cellpixy;
+    auto x = nc_->tcache.cellpixx;
+    std::vector<uint32_t> v(x * y, 0xffffffff);
+    auto ncv = ncvisual_from_rgba(v.data(), y, sizeof(decltype(v)::value_type) * x, x);
+    REQUIRE(nullptr != ncv);
+    struct ncvisual_options vopts = {
+      .n = nullptr,
+      .scaling = NCSCALE_NONE,
+      .y = 0, .x = 0,
+      .begy = 0, .begx = 0,
+      .leny = y, .lenx = x,
+      .blitter = NCBLIT_PIXEL,
+      .flags = NCVISUAL_OPTION_NODEGRADE,
+    };
+    auto n = ncvisual_render(nc_, ncv, &vopts);
+    REQUIRE(nullptr != n);
+    auto s = n->sprite;
+    REQUIRE(nullptr != s);
+    CHECK(0 == notcurses_render(nc_));
+    CHECK(0 == nc_->tcache.pixel_cell_wipe(s, 0, 0));
+    CHECK(0 == notcurses_render(nc_));
+    CHECK(0 == nc_->tcache.pixel_cell_wipe(s, 1, 1));
+    CHECK(0 == notcurses_render(nc_));
+    CHECK(0 == nc_->tcache.pixel_cell_wipe(s, 1, 0));
+    CHECK(0 == notcurses_render(nc_));
+    CHECK(0 == nc_->tcache.pixel_cell_wipe(s, 0, 1));
+    CHECK(0 == notcurses_render(nc_));
+    ncplane_destroy(n);
+    ncvisual_destroy(ncv);
+    CHECK(0 == notcurses_render(nc_));
+  }
 
   CHECK(!notcurses_stop(nc_));
 }
