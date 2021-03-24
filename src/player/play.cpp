d82a3e935f65117496e3cac0dd073b3efb321c4d
nick black <dankamongmen@gmail.com>
2021-03-24 05:18:01 -0400

    [ncplayer] erase() standard plane for frame count

--- src/player/play.cpp
+++ src/player/play.cpp
@@ -66,7 +66,9 @@ auto perframe(struct ncvisual* ncv, struct ncvisual_options* vopts,
     marsh->blitter = ncvisual_media_defblitter(nc, vopts->scaling);
   }
   if(!marsh->quiet){
-    stdn->printf(0, NCAlign::Left, "frame %06d (%s)    ", marsh->framecount,
+    // FIXME put this on its own plane if we're going to erase()ing it
+    stdn->erase();
+    stdn->printf(0, NCAlign::Left, "frame %06d (%s)", marsh->framecount,
                  notcurses_str_blitter(vopts->blitter));
   }
   char* subtitle = ncvisual_subtitle(ncv);
