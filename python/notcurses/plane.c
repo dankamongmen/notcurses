595fab7769385bfafdc3362256c7c05b8d772d27
igo95862 <igo95862@yandex.ru>
2021-04-17 12:14:27 +0300

    python: Add NcPlane initializers

--- python/notcurses/plane.c
+++ python/notcurses/plane.c
@@ -35,8 +35,51 @@ NcPlane_new(PyTypeObject *Py_UNUSED(subtype), PyObject *Py_UNUSED(args), PyObjec
     return NULL;
 }
 
-static PyMethodDef NcPlane_methods[] = {
+static PyObject *
+Ncplane_create(NcPlaneObject *self, PyObject *args, PyObject *kwds)
+{
+    int y = 0, x = 0;
+    int rows = 0, cols = 0;
+    const char *name = NULL;
+    // TODO reseize callback
+    unsigned long long flags = 0;
+    int margin_b = 0, margin_r = 0;
 
+    char *keywords[] = {"y_pos", "x_pos",
+                        "rows", "cols",
+                        "name",
+                        "flags",
+                        "margin_b", "margin_r", NULL};
+
+    GNU_PY_CHECK_INT(PyArg_ParseTupleAndKeywords(args, kwds, "|ii ii s K ii", keywords,
+                                                 &y, &x,
+                                                 &rows, &cols,
+                                                 &name,
+                                                 &flags,
+                                                 &margin_b, &margin_r));
+
+    ncplane_options options = {
+        .y = y,
+        .x = x,
+        .rows = rows,
+        .cols = cols,
+        .name = name,
+        .flags = (uint64_t)flags,
+        .margin_b = margin_b,
+        .margin_r = margin_r,
+    };
+
+    PyObject *new_object CLEANUP_PY_OBJ = NcPlane_Type.tp_alloc((PyTypeObject *)&NcPlane_Type, 0);
+    NcPlaneObject *new_plane = (NcPlaneObject *)new_object;
+    new_plane->ncplane_ptr = CHECK_NOTCURSES_PTR(ncplane_create(self->ncplane_ptr, &options));
+    new_plane->is_stdplane = false;
+
+    Py_INCREF(new_object);
+    return new_object;
+}
+
+static PyMethodDef NcPlane_methods[] = {
+    {"create", (void *)Ncplane_create, METH_VARARGS | METH_KEYWORDS, PyDoc_STR("Create a new ncplane bound to plane 'n', at the offset 'y'x'x' (relative to the origin of 'n') and the specified size. The number of 'rows' and 'cols' must both be positive. This plane is initially at the top of the z-buffer, as if ncplane_move_top() had been called on it. The void* 'userptr' can be retrieved (and reset) later. A 'name' can be set, used in debugging.")},
     {NULL, NULL, 0, NULL},
 };
 
