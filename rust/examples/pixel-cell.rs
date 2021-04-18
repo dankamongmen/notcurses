08a3edb71fac908eb8147b20e4b81a11c5baef11
joseLuís <joseluis@andamira.net>
2021-04-18 11:33:58 +0200

    [rust] update pixel-cell example; #1546

--- rust/examples/pixel-cell.rs
+++ rust/examples/pixel-cell.rs
@@ -38,15 +38,14 @@ fn main() -> NcResult<()> {
     }
 
     // show the newly created ncvisual delimited with the box drawing characters
-    let pixels = NcVisual::from_rgba(buffer.as_slice(), pg.cell_y, pg.cell_x * 4, pg.cell_x)?;
+    let mut pixels = NcVisual::from_rgba(buffer.as_slice(), pg.cell_y, pg.cell_x * 4, pg.cell_x)?;
     let voptions =
         NcVisualOptions::without_plane(1, 2, 0, 0, pg.cell_y, pg.cell_x, NCBLIT_PIXEL, 0, 0);
     pixels.render(&mut nc, &voptions)?;
-    rsleep![&mut nc, 2];
+    rsleep![&mut nc, 1];
 
     // show the ncvisual, scaled
-    // FIXME: use nearest neighbor interpolation for scale
-    let mut vplane2 = NcPlane::new_bound(&mut stdplane, 4, 4, 8, 8)?;
+    let mut vplane2 = NcPlane::new_bound(&mut stdplane, 4, 4, 4, 4)?;
     let voptions2 = NcVisualOptions::with_plane(
         &mut vplane2,
         NCSCALE_SCALE,
@@ -61,6 +60,22 @@ fn main() -> NcResult<()> {
         0,
     );
     pixels.render(&mut nc, &voptions2)?;
+    rsleep![&mut nc, 1];
+
+    // show the ncvisual, inflated
+    let voptions3 = NcVisualOptions::without_plane(
+        4,
+        10,
+        0,
+        0,
+        pg.cell_y,
+        pg.cell_x,
+        NCBLIT_PIXEL,
+        0,
+        0,
+    );
+    pixels.inflate(2)?; // FIXME doesn't work (try different values)
+    pixels.render(&mut nc, &voptions3)?;
     rsleep![&mut nc, 2];
 
     pixels.destroy();
