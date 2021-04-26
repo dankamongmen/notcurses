1d9ae4082fd0122666f978793ae85ea82fb1168f
nick black <dankamongmen@gmail.com>
2021-04-26 02:15:43 -0400

    kitty: fix b64idx(), fixing rebuilding #1440

--- src/lib/kitty.c
+++ src/lib/kitty.c
@@ -38,9 +38,9 @@ b64idx(char b64){
   if(b64 >= 'A' && b64 <= 'Z'){
     return b64 - 'A';
   }else if(b64 >= 'a' && b64 <= 'z'){
-    return b64 - 'a';
+    return b64 - 'a' + 26;
   }else if(b64 >= '0' && b64 <= '9'){
-    return b64 - '0';
+    return b64 - '0' + 52;
   }else if(b64 == '+'){
     return 62;
   }else{
