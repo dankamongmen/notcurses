d33956d6c77a98753cb63c81486862e818e1d5e8
nick black <dankamongmen@gmail.com>
2021-05-08 18:26:57 -0400

    update rust docs to 2.2.11

--- rustdoc/implementors/core/convert/trait.AsMut.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["libnotcurses_sys"] = [{"text":"impl&lt;'a&gt; AsMut&lt;ncdirect&gt; for NcD&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; AsMut&lt;notcurses&gt; for Nc&lt;'a&gt;","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- rustdoc/implementors/core/ops/deref/trait.Deref.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["libnotcurses_sys"] = [{"text":"impl&lt;'a&gt; Deref for NcD&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Deref for Nc&lt;'a&gt;","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
