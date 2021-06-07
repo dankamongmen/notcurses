0d7b6619bb92b18fac90328656ce2430beb62f0b
joseLuís <joseluis@andamira.net>
2021-06-07 20:53:42 +0200

    [rust] add example for issue #1732

---
+++ rust/examples/issue-1732.rs
@@ -0,0 +1,26 @@
+use libnotcurses_sys::*;
+
+fn main() -> NcResult<()> {
+    let mut nc = Notcurses::new()?;
+
+    let chan_blue = NcChannelPair::with_rgb(0x88aa00, 0x2222287);
+    let chan_green = NcChannelPair::with_rgb(0x224411, 0x229922);
+
+    // FIXME: this doesn't show at all :/
+    let plane1 = NcPlane::new(&mut nc, 0, 0, 20, 40)?;
+    plane1.set_base("·", 0, chan_green)?;
+    let plane2 = NcPlane::new_bound(plane1, 0, 0, 2, 4)?;
+    plane2.set_base("·", 0, chan_blue)?;
+    plane1.render()?;
+    plane1.rasterize()?;
+    rsleep![&mut nc, 2];
+
+    // but the stdplane does show
+    nc.stdplane().set_base("x", 0, chan_blue)?;
+    let substdplane = NcPlane::new_bound(nc.stdplane(), 5, 10, 10, 20)?;
+    substdplane.set_base("~", 0, chan_green)?;
+    rsleep![&mut nc, 2];
+
+    nc.stop()?;
+    Ok(())
+}
