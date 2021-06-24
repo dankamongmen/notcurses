ce073d22ac44b4ead838fb6781fea9d0bfc0db72
nick black <dankamongmen@gmail.com>
2021-06-23 22:09:33 -0400

    fallin demo: no write to failed alloc

--- src/demo/fallin.c
+++ src/demo/fallin.c
@@ -97,6 +97,9 @@ int fallin_demo(struct notcurses* nc){
   struct ncplane* stdn = notcurses_stddim_yx(nc, &dimy, &dimx);
   size_t usesize = sizeof(bool) * dimy * dimx;
   bool* usemap = malloc(usesize);
+  if(usemap == NULL){
+    return -1;
+  }
   memset(usemap, 0, usesize);
   // brick size is relative to the screen size
   const int maxx = dimx > 39 ? dimx / 10 : 2;
