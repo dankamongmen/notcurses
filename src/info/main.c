b1fd24a9701d9a04ce72a895094be44d5996ab7c
nick black <dankamongmen@gmail.com>
2021-06-24 19:12:16 -0400

    notcurses-info: use some coloring #1823

--- src/info/main.c
+++ src/info/main.c
@@ -20,6 +20,7 @@ capbool(const tinfo* ti, bool cap){
 
 static int
 unicodedumper(const struct notcurses* nc, struct ncplane* n, tinfo* ti, const char* indent){
+  ncplane_set_fg_rgb(n, 0x9172ec);
   ncplane_printf(n, "%sutf8%lc quad%lc sex%lc braille%lc images%lc videos%lc\n",
                  indent,
                  capbool(ti, ti->caps.utf8),
@@ -28,7 +29,14 @@ unicodedumper(const struct notcurses* nc, struct ncplane* n, tinfo* ti, const ch
                  capbool(ti, ti->caps.braille),
                  capbool(ti, notcurses_canopen_images(nc)),
                  capbool(ti, notcurses_canopen_videos(nc)));
+  ncplane_set_fg_default(n);
   if(ti->caps.utf8){
+    /*uint32_t l = CHANNEL_RGB_INITIALIZER(0x20, 0x20, 0x20);
+    uint32_t r = CHANNEL_RGB_INITIALIZER(0x80, 0x80, 0x80);
+    int y, x;
+    ncplane_cursor_yx(n, &y, &x);
+    ncplane_highgradient_sized(n, l, r, l, r, 10, ncplane_dim_x(n));
+    ncplane_cursor_move_yx(n, y, x);*/
     ncplane_printf(n, " {%ls} {%ls} ⎧%.122ls⎫        ⎧█ ⎫ 🯰🯱\n",
                    NCHALFBLOCKS, NCQUADBLOCKS, NCSEXBLOCKS);
     ncplane_printf(n, "                           ⎩%ls⎭        ⎪🮋▏⎪ 🯲🯳\n",
@@ -57,6 +65,7 @@ unicodedumper(const struct notcurses* nc, struct ncplane* n, tinfo* ti, const ch
 
 static void
 tinfo_debug_bitmaps(struct ncplane* n, const tinfo* ti, const char* indent){
+  ncplane_set_fg_rgb(n, 0x5efa80);
   if(!ti->pixel_draw){
     ncplane_printf(n, "%sdidn't detect bitmap graphics support\n", indent);
   }else if(ti->sixel_maxy){
@@ -85,6 +94,8 @@ capyn(const tinfo* ti, const char* cap){
 
 static void
 tinfo_debug_caps(struct ncplane* n, const tinfo* ti, const char* indent){
+  ncplane_putchar(n, '\n');
+  ncplane_set_fg_rgb8(n, 0xcc, 0x99, 0xff);
   ncplane_printf(n, "%srgb%lc ccc%lc af%lc ab%lc appsync%lc u7%lc cup%lc vpa%lc hpa%lc sgr%lc sgr0%lc op%lc fgop%lc bgop%lc\n",
           indent,
           capbool(ti, ti->caps.rgb),
@@ -101,12 +112,15 @@ tinfo_debug_caps(struct ncplane* n, const tinfo* ti, const char* indent){
           capyn(ti, get_escape(ti, ESCAPE_OP)),
           capyn(ti, get_escape(ti, ESCAPE_FGOP)),
           capyn(ti, get_escape(ti, ESCAPE_BGOP)));
+  ncplane_set_fg_rgb8(n, 0xc4, 0x5a, 0xec);
   ncplane_printf(n, "%sbackground of 0x%06lx is %sconsidered transparent\n", indent, ti->bg_collides_default & 0xfffffful,
                    (ti->bg_collides_default & 0x01000000) ? "" : "not ");
+  ncplane_set_fg_default(n);
 }
 
 static void
 tinfo_debug_styles(struct ncplane* n, const char* indent){
+  ncplane_set_fg_rgb8(n, 0xc8, 0xa2, 0xc8);
   ncplane_putstr(n, indent);
   tinfo_debug_style(n, "bold", NCSTYLE_BOLD);
   ncplane_putchar(n, ' ');
