505f38956db93a738233687c3324ff2cf49f2848
nick black <dankamongmen@gmail.com>
2021-07-03 21:52:32 -0400

    Fade unit test: use CELL_CHAR_INITIALIZER

--- src/tests/fade.cpp
+++ src/tests/fade.cpp
@@ -41,8 +41,7 @@ TEST_CASE("Fade") {
   REQUIRE(0 == ncplane_cursor_move_yx(n_, 0, 0));
   int dimy, dimx;
   ncplane_dim_yx(n_, &dimy, &dimx);
-  nccell c = CELL_TRIVIAL_INITIALIZER;
-  c.gcluster = '*';
+  nccell c = CELL_CHAR_INITIALIZER('*');
   nccell_set_fg_rgb8(&c, 0xff, 0xff, 0xff);
   unsigned rgb = 0xffffffu;
   CHECK(!ncplane_set_scrolling(n_, true));
