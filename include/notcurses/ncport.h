96fb4d75bfc6b3004b1473c8ca0adb551d609355
nick black <dankamongmen@gmail.com>
2021-07-21 22:55:56 -0400

    add ncport.h

---
+++ include/notcurses/ncport.h
@@ -0,0 +1,41 @@
+#ifndef NOTCURSES_NCPORT
+#define NOTCURSES_NCPORT
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// take host byte order and turn it into network (reverse on LE, no-op on BE),
+// then reverse that, guaranteeing LE. htole(x) == ltohe(x).
+#if defined(__linux__) || defined(__gnu_hurd__)
+#include <poll.h>
+#include <termios.h>
+#include <byteswap.h>
+#include <netinet/in.h>
+#define htole(x) (__bswap_32(htonl(x)))
+#elif defined(__APPLE__)
+#include <poll.h>
+#include <termios.h>
+#include <netinet/in.h>
+#include <libkern/OSByteOrder.h>
+#define htole(x) (OSSwapInt32(htonl(x)))
+#elif defined(__MINGW64__)
+#include <winsock.h>
+#include <winsock2.h>
+#define htole(x) (_byteswap_ulong(htonl(x)))
+#include <wingdi.h>
+#define wcwidth(w) 1
+#define wcswidth(w, s) (s)
+#define sigset_t int
+#else // bsd
+#include <poll.h>
+#include <netinet/in.h>
+#include <sys/endian.h>
+#define htole(x) (bswap32(htonl(x)))
+#endif
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif
