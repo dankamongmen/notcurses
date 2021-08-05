4fa785e3d0dcd49d3aad6c0bc47fdc7611a54c7f
nick black <dankamongmen@gmail.com>
2021-08-05 11:00:52 -0400

    windows terminal: add ESCAPE_NOBOLD

--- src/lib/windows.c
+++ src/lib/windows.c
@@ -25,12 +25,13 @@ int prepare_windows_terminal(tinfo* ti, size_t* tablelen, size_t* tableused){
     { ESCAPE_CNORM, "\x1b[?25h", },
     { ESCAPE_U7,    "\x1b[6n", },
     { ESCAPE_BOLD,  "\x1b[1m", },
+    { ESCAPE_NOBOLD,"\x1b[22m", },
     { ESCAPE_SITM,  "\x1b[3m", },
     { ESCAPE_RITM,  "\x1b[23m", },
     { ESCAPE_SMUL,  "\x1b[4m", },
     { ESCAPE_RMUL,  "\x1b[24m", },
     { ESCAPE_SMULX, "\x1b[4:3m", },
-    { ESCAPE_SMULNOX,  "\x1b[4:0m", },
+    { ESCAPE_SMULNOX,"\x1b[4:0m", },
     { ESCAPE_SGR0,  "\x1b[0m", },
     { ESCAPE_MAX, NULL, }
   }, *w; 
