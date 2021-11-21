43de0ae3a5efe8f57c7161d9c1aa8bdf558d9941
nick black <dankamongmen@gmail.com>
2021-11-21 00:04:40 -0500

    [oiio] update declaration to match definition/contract

--- src/media/oiio.h
+++ src/media/oiio.h
@@ -16,7 +16,7 @@ int oiio_blit(ncvisual* ncv, int rows, int cols,
               const blitterargs* bargs);
 ncvisual* oiio_from_file(const char* filename);
 int oiio_decode_loop(ncvisual* ncv);
-int oiio_resize(ncvisual* nc, int rows, int cols);
+int oiio_resize(ncvisual* nc, unsigned rows, unsigned cols);
 ncvisual* oiio_create(void);
 void oiio_destroy(ncvisual* ncv);
 int oiio_blit_dispatch(struct ncplane* nc, const struct blitset* bset,
