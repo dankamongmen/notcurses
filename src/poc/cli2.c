50fbb0a778100c7e3a523e46b44ba3bf5048ad54
nick black <dankamongmen@gmail.com>
2021-11-24 03:19:48 -0500

    [cli2 PoC] strip trailing whitespace

--- src/poc/cli2.c
+++ src/poc/cli2.c
@@ -1,17 +1,17 @@
 #include <notcurses/notcurses.h>
 
-int main(void){                                                                                                                     
-  struct notcurses_options opts = {                                                                                                 
-    .flags = NCOPTION_NO_ALTERNATE_SCREEN |                                                                                         
-             NCOPTION_PRESERVE_CURSOR |                                                                                             
-             NCOPTION_NO_CLEAR_BITMAPS |                                                                                            
-             NCOPTION_DRAIN_INPUT,                                                                                                  
-  };                                                                                                                                
-  struct notcurses* nc = notcurses_init(&opts, NULL);                                                                               
-  if(nc == NULL){                                                                                                                   
-    return EXIT_FAILURE;                                                                                                            
-  }                                                                                                                                 
+int main(void){
+  struct notcurses_options opts = {
+    .flags = NCOPTION_NO_ALTERNATE_SCREEN |
+             NCOPTION_PRESERVE_CURSOR |
+             NCOPTION_NO_CLEAR_BITMAPS |
+             NCOPTION_DRAIN_INPUT,
+  };
+  struct notcurses* nc = notcurses_init(&opts, NULL);
+  if(nc == NULL){
+    return EXIT_FAILURE;
+  }
   notcurses_render(nc);
-  notcurses_stop(nc);                                                                                                               
-  return EXIT_SUCCESS;                                                                                                              
+  notcurses_stop(nc);
+  return EXIT_SUCCESS;
 }
