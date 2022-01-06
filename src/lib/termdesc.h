59eb7dd18bff2780ccf7da60315db4859f8f80b9
nick black <dankamongmen@gmail.com>
2022-01-05 22:20:10 -0500

    [XTMODKEYS] undo with m, not n #2526

--- src/lib/termdesc.h
+++ src/lib/termdesc.h
@@ -24,7 +24,7 @@ extern "C" {
 // disable key modifier options; this corresponds to a resource value of
 // "-1", which cannot be set with the [>m sequence. supposedly, "[>m" by
 // itself ought reset all of them, but this doesn't seem to work FIXME.
-#define XTMODKEYSUNDO "\x1b[>2n\x1b[>4n"
+#define XTMODKEYSUNDO "\x1b[>2m\x1b[>4m"
 
 struct ncpile;
 struct sprixel;
