a924cede7fe8da1df675e83952661cfbaaa87a43
nick black <dankamongmen@gmail.com>
2025-01-02 00:29:48 -0500

    ffmpeg: relax version check for AVFrame->duration #2828

--- src/media/ffmpeg.c
+++ src/media/ffmpeg.c
@@ -41,7 +41,7 @@ typedef struct ncvisual_details {
 #define IMGALLOCALIGN 64
 
 uint64_t ffmpeg_pkt_duration(const AVFrame* frame){
-#if LIBAVUTIL_VERSION_MAJOR < 59
+#if LIBAVUTIL_VERSION_MAJOR < 58
       return frame->pkt_duration;
 #else
       return frame->duration;
